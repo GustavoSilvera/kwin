4e534d920b247d5ec7daa09d2c7f0585b4166292
Vlad Zahorodnii <vlad.zahorodnii@kde.org>
2021-07-08 10:31:56 +0300

    scene: Drop KrkTexture::bind()
    The only reason why it existed was so that we can re-attach X11 pixmaps
    to textures. Now that it's done in a different fashion, the bind method
    is unused and can be removed.

--- src/plugins/scenes/opengl/scene_opengl.cpp
+++ src/plugins/scenes/opengl/scene_opengl.cpp
@@ -1127,9 +1127,10 @@ void OpenGLWindow::performPaint(int mask, const QRegion &region, const WindowPai
             opacity = renderNode.opacity;
         }
 
-        texture->setFiltering(KrkTexture::Linear);
-        texture->setWrapMode(KrkTexture::ClampToEdge);
-        texture->bind();
+        GLTexture *nativeTexture = sceneTextureToGLTexture(texture);
+        nativeTexture->setFilter(texture->filtering() == KrkTexture::Linear ? GL_LINEAR : GL_NEAREST);
+        nativeTexture->setWrapMode(texture->wrapMode() == KrkTexture::Repeat ? GL_REPEAT : GL_CLAMP_TO_EDGE);
+        nativeTexture->bind();
 
         vbo->draw(region, primitiveType, renderNode.firstVertex,
                   renderNode.vertexCount, renderContext.hardwareClipping);
